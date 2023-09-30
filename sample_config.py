67ec1a31bdb2be153f37f2237b3cdb06d1cb7971
Si Arab - <98678420+fadhilabdat04@users.noreply.github.com>
2023-09-30 14:10:38 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,12 +1,12 @@
-BOT_TOKEN = "6230205853:AAGrethJAZtt7keAL1JRexwjU9Bmj3wIcvg"
+BOT_TOKEN = "6484534684:AAFhc381-zhXgXLXG9eNf37ED4DDafZc1yc"
 API_ID = 28220146
 SESSION_STRING = "BQAhIHQAwqe5VyiV-qo_oaawFjYL76-JuVpiTjnQtaVsQYAE01WZu0AByiBlsieEmxKBaFJB8SuCIypVVcKNj6RA2qK4u2s4nQ27BUAT_KKbBNYTyMNZphoKRT06kLEaozZDESbO0RD-bmgtEcrSYAXyj_SisTrlSbE86x6YGkDyiQ2hAq6WL8QX0Uj_L6aSngul9UE9S1H_Ggx4ulGGLtZjo2heGOGL4BhnXkwJzRpM5lRM2014drsej4Zz8UhvUE6UpxVBJjklBVbjRNBfoiVyOzXtsRC4umn4QSgFmK9pewaTS69peCc0ZIJUsnA8BoKJf16bMYS5oEw1I1PH27YJR71uKwAAAAButFe5AA" # Check Readme to generate sessions; need for userbot
 API_HASH = "1d1e1da63ea443b083df5b69d084f82f"
 USERBOT_PREFIX = "/"
 PHONE_NUMBER = "+6285710970878"  # Need for Userbot
-SUDO_USERS_ID = [1385170049 , 5719914585]  # Sudo users have full access to everything, don't trust anyone
-LOG_GROUP_ID = -1001896097424
-GBAN_LOG_GROUP_ID = -1001896097424
+SUDO_USERS_ID = [1948147616]  # Sudo users have full access to everything, don't trust anyone
+LOG_GROUP_ID = -1001795374467
+GBAN_LOG_GROUP_ID = -1001795374467
 MESSAGE_DUMP_CHAT = 0
 WELCOME_DELAY_KICK_SEC = 0
 MONGO_URL = "mongodb+srv://Swan:Swan@cluster0.hzzfpih.mongodb.net/?retryWrites=true&w=majority"
